195ec6fa8cb8316e49f7c0bcab5d9293724145e3
Lucas Owino <lucowish35@gmail.com>
2021-06-14 01:46:52 +0300

    added multiselect plugin

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+
+var params = { sortable: true };
+$("select#demo1").treeMultiselect({searchable: true, searchParams: ['section', 'text']});
+function treeOnChange(allSelectedItems, addedItems, removedItems) {
+    console.log("something changed!");
+  }
+  
+  $("select").treeMultiselect({
+    allowBatchSelection: false,
+    onChange: treeOnChange,
+    startCollapsed: true
+  });
+
+// $("select#demo1").treeMultiselect({searchable: true, searchParams: ['section', 'text']});
